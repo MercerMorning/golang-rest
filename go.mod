6eb7ba182adb979515c3e3d24be137f1178c05fe
loremercer <hugopochta@gmail.com>
2024-03-23 22:48:43 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module rest
+
+go 1.22
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)
---
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -0,0 +1,15 @@
+package apiserver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAPIServer_HandleHello(t *testing.T) {
+	s := New(NewConfig())
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
+	s.handleHello().ServeHTTP(rec, req)
+	//assert.Equal(t, rec.Body.String(), "hello")
+}
