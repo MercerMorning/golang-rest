4081af5b2a30c4bab67573aad26d8b3491283617
loremercer <hugopochta@gmail.com>
2024-03-24 12:11:22 +0300

    added repo

---
+++ internal/app/store/store_test.go
@@ -0,0 +1,19 @@
+package store_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "host=localhost dbname=database sslmode=disable user=user password=password"
+	}
+
+	os.Exit(m.Run())
+}
